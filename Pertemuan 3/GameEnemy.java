98c18c1de29313d2fcab6c2547170af78a2a7425
Dio <63654933+diofajriyanto@users.noreply.github.com>
2021-03-23 23:39:35 +0700

    Create GameEnemy.java

---
+++ Pertemuan 3/GameEnemy.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgame;
+
+/**
+ *
+ * @author DIO
+ */
+public class GameEnemy {
+    private double width;
+    private double height;
+    private int positionX;
+    private int positionY;
+    
+    GameEnemy() {
+           
+    }
+    
+    public GameEnemy(double width,double height,int positionX,int positionY){
+           
+    }
+    
+    public void setDimension(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+    
+    public void setPosition(int positionX, int positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+    
+    public double getwidth (){
+        return this.width;
+    }
+    
+    public double getheight(){
+        return this.height;
+    }
+    
+    public int getX(){
+        return this.positionX;   
+    }
+    
+    public int getY(){
+        return this.positionY;
+    }
+    
+    public void run(){
+        System.out.println("Running: ");
+    }
+}
